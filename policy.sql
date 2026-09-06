28acda8d1c417db8c34d0ce66bd426b04bcfb167
Divyanelli <divyanelli14@gmail.com>
2026-09-06 23:15:06 +0530

    added claims field

--- policy.sql
+++ policy.sql
@@ -2,7 +2,8 @@ CREATE TABLE policy (
     policy_id INT,
     policy_number VARCHAR(50),
     customer_id INT,
-    premium DECIMAL(10,2)
+    premium DECIMAL(10,2),
+    claim BOOLEAN
 );
 
 INSERT INTO policy VALUES
