cf8058e00ff6a243189a3814ae63dca7b6874416
Divyanelli <divyanelli14@gmail.com>
2026-09-06 19:33:41 +0530

    Created Quote and Policy table

---
+++ policy.sql
@@ -0,0 +1,6 @@
+CREATE TABLE policy (
+    policy_id INT,
+    policy_number VARCHAR(50),
+    customer_id INT,
+    premium DECIMAL(10,2)
+);
---
+++ quote.sql
@@ -0,0 +1,5 @@
+CREATE TABLE quote (
+    quote_id INT,
+    customer_id INT,
+    quote_amount DECIMAL(10,2)
+);
